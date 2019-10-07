df60c1966caaeaa6fc56ce8afdc010569db76302
ShAM <sham@istranet.ru>
2019-10-07 05:25:05 +0300

    add highlight for code add fluid for images add access_control for efinder

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,19 +8,36 @@
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/bootstrap.scss');
 require("bootstrap");
+const hljs = require('highlight.js');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
 
 document.addEventListener('DOMContentLoaded', function () {
-        var pre = document.getElementsByTagName('code');
-        for (var i = 0; i < pre.length; i++) {
-            pre[i].innerHTML = '<span class="line-number pl-1"></span>' + pre[i].innerHTML + '<span class="cl"></span>';
-            let arr = pre[i].className.split("-");
-            pre[i].innerHTML = "<div class=\"bg-dark text-white language p-2\">" + arr[1].toUpperCase() + '</div>' + pre[i].innerHTML;
-            var num = pre[i].innerHTML.split(/\n/).length;
-            for (var j = 0; j < num; j++) {
-                var line_num = pre[i].getElementsByTagName('span')[0];
-                line_num.innerHTML += '<span>' + (j + 1) + '</span>';
-            }
+    if (typeof CKEDITOR !== 'undefined') {
+        CKEDITOR.on('instanceReady', function (evt) {
+            evt.editor.dataProcessor.htmlFilter.addRules({
+                elements: {
+                    img: function (el) {
+                        el.addClass('img-fluid');
+                    }
+                }
+            });
+        });
+    }
+    hljs.initHighlightingOnLoad();
+    var pre = document.getElementsByTagName('code');
+    for (var i = 0; i < pre.length; i++) {
+        pre[i].innerHTML = `<span class="line-number pl-1"></span>${pre[i].innerHTML}<span class="cl"></span>`;
+        let arr = pre[i].className.split("-");
+        if(arr[1] === 'cpp')
+            arr[1] = 'c++';
+        if(arr[1] === 'cs')
+            arr[1] = 'c#';
+        pre[i].innerHTML = `<div class="bg-dark text-white language p-2">` + arr[1].toUpperCase() + `</div>${pre[i].innerHTML}`;
+        var num = pre[i].innerHTML.split(/\n/).length;
+        for (var j = 0; j < num; j++) {
+            var line_num = pre[i].getElementsByTagName('span')[0];
+            line_num.innerHTML += '<span>' + (j + 1) + '</span>';
         }
+    }
 });
