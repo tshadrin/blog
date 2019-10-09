13655092fe8a6175cc3c430109efd25f5ff7761d
ShAM <sham@istranet.ru>
2019-10-09 12:42:11 +0300

    Исправлены ошибки в js Исправлены ошибки разметки.

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,13 +29,15 @@ document.addEventListener('DOMContentLoaded', function () {
     for (var i = 0; i < pre.length; i++) {
         pre[i].innerHTML = `<span class="line-number pl-1"></span>${pre[i].innerHTML}<span class="cl"></span>`;
         let arr = pre[i].className.split("-");
-        if(arr[1] === 'cpp') {
-            arr[1] = 'c++';
+        if (typeof arr[1] !== 'undefined') {
+            if (arr[1] === 'cpp') {
+                arr[1] = 'c++';
+            }
+            if (arr[1] === 'cs') {
+                arr[1] = 'c#';
+            }
+            pre[i].innerHTML = `<div class="bg-dark text-white language p-2">` + arr[1].toUpperCase() + `</div>${pre[i].innerHTML}`;
         }
-        if(arr[1] === 'cs') {
-            arr[1] = 'c#';
-        }
-        pre[i].innerHTML = `<div class="bg-dark text-white language p-2">` + arr[1].toUpperCase() + `</div>${pre[i].innerHTML}`;
         var num = pre[i].innerHTML.split(/\n/).length;
         for (var j = 0; j < num; j++) {
             var line_num = pre[i].getElementsByTagName('span')[0];
