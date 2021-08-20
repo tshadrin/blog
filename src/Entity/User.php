8ffdd0a44e97f12c21e2397038f83fcaf1519977
ShAM <sham@istranet.ru>
2021-08-20 12:21:21 +0300

    fix removed comment

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
-
+/**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  * @ORM\Table(name="users")
  */
