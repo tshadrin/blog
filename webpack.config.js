a7f524189a9c67bcac404f84ca14ee739ae0cbe5
ShAM <sham@istranet.ru>
2022-01-23 03:28:53 +0300

    add youtube plugin to ckeditor

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,8 @@ Encore
         {from: './node_modules/ckeditor/adapters', to: 'ckeditor/adapters/[path][name].[ext]'},
         {from: './node_modules/ckeditor/lang', to: 'ckeditor/lang/[path][name].[ext]'},
         {from: './node_modules/ckeditor/plugins', to: 'ckeditor/plugins/[path][name].[ext]'},
-        {from: './node_modules/ckeditor/skins', to: 'ckeditor/skins/[path][name].[ext]'}
+        {from: './node_modules/ckeditor/skins', to: 'ckeditor/skins/[path][name].[ext]'},
+        {from: './node_modules/ckeditor-youtube-plugin/youtube/', to: 'youtube/[path][name].[ext]'}
     ])
     // enables Sass/SCSS support
     .enableSassLoader()
