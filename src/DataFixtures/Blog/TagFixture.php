1e9ae56c5f17d31840e250563e06dc115380da48
sham <reg@shadrin.me>
2022-02-28 08:03:49 +0300

    add fixtures

---
+++ src/DataFixtures/Blog/TagFixture.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataFixtures\Blog;
+
+use App\Entity\Blog\Tag;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class TagFixture extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('ru_RU');
+        for ($i = 0; $i < 100; $i++) {
+            $tag = new Tag(name: $faker->word());
+            $manager->persist($tag);
+            $this->addReference(name: "tag{$i}", object: $tag);
+        }
+        $manager->flush();
+    }
+}
